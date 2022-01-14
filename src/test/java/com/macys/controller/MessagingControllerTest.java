0f84820c3644138646bb58245c9490f01e207b82
Udaykumar <udayappdeveloper@gmail.com>
2022-01-14 21:51:59 +0530

    cleaned code
    cleaned code

--- src/test/java/com/macys/controller/MessagingControllerTest.java
+++ src/test/java/com/macys/controller/MessagingControllerTest.java
@@ -9,10 +9,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
@@ -20,7 +18,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -30,8 +27,6 @@ import com.macys.dto.json.OrderMessageJson;
 import com.macys.dto.xml.FulfillmentOrder;
 import com.macys.service.MessagingService;
 
-/*@RunWith(SpringRunner.class)
-@WebMvcTest(value = MessagingController.class)*/
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
